271c5b2807c0fd14f006c8d29607cc2cbd424bfc
Sam Osborne <sam.tosborne@googlemail.com>
2016-10-17 14:32:01 +0100

    Add comments

--- _bin/hugo-017.sh
+++ _bin/hugo-017.sh
@@ -3,6 +3,10 @@
 mkdir ~/hugo
 cd hugo
 wget https://github.com/spf13/hugo/releases/download/v0.17/hugo_0.17_Linux-64bit.tar.gz
+echo "Downloaded hugo"
 tar -xgzf hugo*tar.gz
+echo "Extracted hugo"
 cd hugo*/
 mv hugo* ~/
+echo "Moved hugo to home directory"
+echo "Exiting"
